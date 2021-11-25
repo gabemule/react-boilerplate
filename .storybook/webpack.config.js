66ec8606d48cc78889413f8786957a01cd150482
Gab Mule <gabemule@gmail.com>
2021-11-25 15:16:52 -0300

    feat: Including storybook support and demonstration stories

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path")
+
+module.exports = ({ config }) => {
+    config.resolve.alias = {
+        "@": path.resolve(__dirname, "..", "src")
+    }
+
+    config.module.rules.push({
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        include: path.resolve(__dirname, '../'),
+    });
+
+    return config
+}
---
+++ src/components/bar/bar.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Foo from '@/components/foo';
+
+const componentProps = {
+    title: "Components/Foo",
+    component: Foo,
+};
+
+
+export default componentProps;
+
+const Template = (args) => <Foo {...args}> Foo Component </Foo>
+
+export const Default = Template.bind({});
